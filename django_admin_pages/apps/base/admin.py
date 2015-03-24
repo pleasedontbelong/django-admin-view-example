703f7de2dfd0e87c52fb65ac2679d2c479389417
Pleasedontbelong <pleasedontbelong@gmail.com>
2015-03-24 21:54:06 +0100

    Add coments

--- django_admin_pages/apps/base/admin.py
+++ django_admin_pages/apps/base/admin.py
@@ -16,25 +16,32 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
 
     def get_urls(self):
+        # this is just a copy paste from the admin code
         def wrap(view):
             def wrapper(*args, **kwargs):
                 return self.admin_site.admin_view(view)(*args, **kwargs)
             return update_wrapper(wrapper, view)
-
+        # get the default urls
         urls = super(UserAdmin, self).get_urls()
-
+        # define my own urls
         my_urls = patterns(
             '',
             url(r'^inactive/$',
                 wrap(self.changelist_view),
                 name="inactive_users")
         )
+        # return the complete list of urls
         return my_urls + urls
 
     def get_changelist(self, request):
+        """
+        This method must return the view to be used for listing the model
+        """
+        # for inactive users use the InactiveUsersView
         if request.resolver_match.url_name == "inactive_users":
             return InactiveUsersView
         return super(UserAdmin, self).get_changelist(request)
 
+# we must unregister the default user model
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
--- django_admin_pages/apps/base/admin_views.py
+++ django_admin_pages/apps/base/admin_views.py
@@ -2,10 +2,14 @@ from django.contrib.admin.views.main import ChangeList
 
 
 class InactiveUsersView(ChangeList):
+    """
+    This view displays the list of inactive users
+    """
     def __init__(self, *args, **kwargs):
         super(InactiveUsersView, self).__init__(*args, **kwargs)
         self.list_display = ('username', 'email', 'date_joined', 'last_login')
 
     def get_queryset(self, request):
         qs = super(InactiveUsersView, self).get_queryset(request)
+        # filter inactive and admin users
         return qs.exclude(is_staff=False).exclude(is_active=False).exclude(is_superuser=False)
